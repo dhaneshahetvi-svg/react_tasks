5b094a77cd0bfc7442d0519fc6c75b26978ffdc1
hetvi <dhaneshahetvi.com>
2026-01-30 13:43:34 +0530

    [DH][added]

---
+++ src/pages/Register.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Register = () => {
+  return (
+    <>
+    <div>
+        <div className="from-container"></div>
+    </div>
+    </>
+  );
+};
+
+export default Register;
